69fd0711d22ed440b4abe36389cb875f1c034d36
Ibe Emeka Eric 🧑‍💻 <38852525+eric5509@users.noreply.github.com>
2024-10-24 23:49:02 +0100

    Update Input.tsx

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,17 +1,17 @@
 type Props = {
-  label?: "";
-  subLabel?: "";
-  placeholder?:""
-};
-
-export default function Input({ label, subLabel, placeholder }: Props) {
-  return (
-    <div>
-      <p className="mb-2">{label ?? "Qty"}</p>
-      <div className="h-12 bg-white relative border-2 rounded-xl w-64">
-        <input type="text" className="bg-transparent pl-3 w-full h-full outline-none" placeholder={placeholder}/>
-        <p className="absolute right-2 -translate-y-1/2 top-1/2">{subLabel ?? "USDT"}</p>
+    label?: string;
+    subLabel?: string;
+    placeholder?: string
+  };
+  
+  export default function Input2({ label, subLabel, placeholder }: Props) {
+    return (
+      <div>
+        <p className="mb-2">{label ?? "Qty"}</p>
+        <div className="h-12 bg-white relative border-2 rounded-xl w-64">
+          <input type="text" className="bg-transparent pl-3 w-full h-full outline-none" placeholder={placeholder}/>
+          <p className="absolute right-2 -translate-y-1/2 top-1/2">{subLabel ?? "USDT"}</p>
+        </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
